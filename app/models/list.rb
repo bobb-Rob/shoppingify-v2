d57d98a4c484753f80352d082101ce6208ce1dc0
Robertson Akpan <robertsonakpan@gmail.com>
2023-01-19 13:09:25 +0100

    add status validation to List model - user can only have one ative list but many cancelled or completed list

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,4 +4,9 @@ class List < ApplicationRecord
   has_many :items, through: :records
 
   validates :name, presence: true, uniqueness: { scope: :user_id }
+  validates :status, uniqueness: { scope: :user_id }, unless: :value_is_cancel_or_complete?
+
+  def value_is_cancel_or_complete?
+    (status == "cancel") || (status == "complete")
+  end
 end
