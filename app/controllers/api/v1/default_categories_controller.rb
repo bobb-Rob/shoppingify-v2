f187f79967fdf79d57fa227175d59ca773db560d
Robertson Akpan <robertsonakpan@gmail.com>
2023-03-13 12:54:11 +0100

    Create route in DefaultCategoriesController update with success and failure message

--- app/controllers/api/v1/default_categories_controller.rb
+++ app/controllers/api/v1/default_categories_controller.rb
@@ -4,69 +4,27 @@ class Api::V1::DefaultCategoriesController < ApiController
     render json: DefaultCategory.all, only: %i[name id]
   end
 
-  # def create
-  #   # Receive an array of default_category_ids from params and, fetch the default_categories from DefaultCategory Model
-  #   # Use catorgory_ids to get the categories
-  #   # then create new Categories with the same name and user_id
-  #   # Create Items associated with fetched default_categories and new categories
+  def create  
+    default_category_ids = params[:selectedCategoriesIds]   
+    if default_category_ids.empty?
+      # return an error message - no default_category_ids were provided, please provide default_category_ids
+      render json: { errors: 'No default_category_ids were provided, please provide default_category_ids' }, status: :unprocessable_entity
+    else
+      default_categories = DefaultCategory.where(id: default_category_ids).includes(:default_items)
+           
+      categories = create_categories(default_categories, current_user.id)    
+  
+      create_items(default_categories, categories, current_user.id)
+  
+      transfer_image_attachments(default_categories)
 
-  #   default_categories = DefaultCategory.where(id: params[:default_category_ids]).includes(:default_items)
-
-  #   default_categories.each do |default_category|
-  #     category = Category.create(name: default_category.name, user_id: current_user.id)
-  #     default_category.default_items.each do |default_item|
-  #       item = Item.new(
-  #         name: default_item.name,
-  #         note: default_item.note,
-  #         image: 'https://media.istockphoto.com/id/171302954/photo/groceries.jpg?s=612x612&w=0&k=20&c=D3MmhT5DafwimcYyxCYXqXMxr1W25wZnyUf4PF1RYw8=',
-  #         category_id: default_category.id,
-  #         user_id: current_user.id)
-
-  #       if default_item.image_attachment.attached?
-  #         begin
-  #           item.image_attachment.attach(default_item.image_attachment.blob)
-  #         rescue => e
-  #           item.errors.add(:image_attachment, "could not be transferred: #{e.message}")
-  #         end
-  #       end
-        
-  #       if item.save
-  #         # Item was saved successfully
-  #       else
-  #         # Handle validation errors and provide appropriate feedback to the user
-          
-  #         # Build a list of error messages
-  #         error_messages = []
-  #         item.errors.full_messages.each do |message|
-  #           error_messages << message
-  #         end
-          
-  #         # Return the error messages as a JSON response
-  #         render json: { errors: error_messages }, status: :unprocessable_entity
-  #       end
-  #     end
-  #   end
-  # end
-
-  def create
-    # parse the params to get the default_category_ids
-    default_category_ids = JSON.parse(params[:default_category_ids])
-    default_categories = DefaultCategory.where(id: default_category_ids).includes(:default_items)
-    puts "default_categories: #{default_categories}"
-    puts "params: #{params}"
-    
-    categories = create_categories(default_categories, current_user.id)
-    # byebug
-
-    create_items(default_categories, categories, current_user.id)
-
-    transfer_image_attachments(default_categories)
+      render json: { message: 'Default categories were created successfully' }, status: :ok
+    end
   end
 
   private
 
   def create_categories(default_categories, user_id)
-    return [] if default_categories.empty?
   
     new_categories = default_categories.map do |default_category|
       Category.new(name: default_category.name, user_id: user_id)
