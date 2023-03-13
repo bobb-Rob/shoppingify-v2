052c1070431b2fd7be8c646a85fb24660483cc99
Robertson Akpan <robertsonakpan@gmail.com>
2023-03-13 03:31:30 +0100

    New category created with id arrays

--- app/controllers/api/v1/default_categories_controller.rb
+++ app/controllers/api/v1/default_categories_controller.rb
@@ -49,11 +49,16 @@ class Api::V1::DefaultCategoriesController < ApiController
   # end
 
   def create
-    default_categories = DefaultCategory.where(id: params[:default_category_ids]).includes(:default_items)
-
+    # parse the params to get the default_category_ids
+    default_category_ids = JSON.parse(params[:default_category_ids])
+    default_categories = DefaultCategory.where(id: default_category_ids).includes(:default_items)
+    puts "default_categories: #{default_categories}"
+    puts "params: #{params}"
+    
     categories = create_categories(default_categories, current_user.id)
+    # byebug
 
-    create_items = create_items(default_categories, categories, current_user.id)
+    create_items(default_categories, categories, current_user.id)
 
     transfer_image_attachments(default_categories)
   end
@@ -61,30 +66,34 @@ class Api::V1::DefaultCategoriesController < ApiController
   private
 
   def create_categories(default_categories, user_id)
+    return [] if default_categories.empty?
+  
     new_categories = default_categories.map do |default_category|
       Category.new(name: default_category.name, user_id: user_id)
     end
     Category.import(new_categories)
-  
+
     new_categories
   end
 
   def create_items(default_categories, categories, user_id)
+    return [] if default_categories.empty?
+
     new_items = []
 
-    default_categories.each do |default_category|
+    default_categories.each_with_index do |default_category, index|
       default_category.default_items.each do |default_item|
         item = Item.new(
           name: default_item.name,
           user_id: user_id,
-          category_id: default_category.id,
+          category_id: categories[index].id,
           note: default_item.note,
           image: 'https://media.istockphoto.com/id/171302954/photo/groceries.jpg?s=612x612&w=0&k=20&c=D3MmhT5DafwimcYyxCYXqXMxr1W25wZnyUf4PF1RYw8='
         )
+
         new_items << item
       end
     end
-  
     Item.import(new_items)
   end
 
@@ -92,7 +101,7 @@ class Api::V1::DefaultCategoriesController < ApiController
     default_categories.each do |default_category|
       default_category.default_items.each do |default_item|
         item = Item.find_by(name: default_item.name)
-  
+          
         if default_item.image_attachment.attached?
           begin
             item.image_attachment.attach(default_item.image_attachment.blob)
