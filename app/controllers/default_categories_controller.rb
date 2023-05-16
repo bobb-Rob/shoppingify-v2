bbd620eab038053fc86c08b846f733f94dd73897
Robertson Akpan <robertsonakpan@gmail.com>
2023-05-16 14:51:28 +0100

    Refactor the index action in the defaultCategory Controller

--- app/controllers/default_categories_controller.rb
+++ app/controllers/default_categories_controller.rb
@@ -1,17 +1,3 @@
-# class DefaultCategoriesController < ApplicationController
-#   def index
-#     # render json: return all categories with array of default items with image url with n+1 query
-#     render json: DefaultCategory.includes(default_items: {image_attachment_attachment: :blob}).map { |category|
-#       category.as_json.merge(
-#         items: category.default_items.map { |item| 
-#           # exclude default_category_id from being added as attribute of item
-#           item.as_json(except: [:default_category_id]).merge(image: url_for(item.image_attachment), category_id: item.default_category.id)
-#           }
-#         )
-#       }
-#   end
-# end
-
 class DefaultCategoriesController < ApplicationController
   def index
     categories_with_items = DefaultCategory.includes(default_items: {image_attachment_attachment: :blob}).map do |category|
